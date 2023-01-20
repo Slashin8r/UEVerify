c6097f9c3001c283620313fcb26bbc9c920ec610
Slashin8r <jeklund86@gmail.com>
2023-01-20 10:01:43 -0600

    Added: Comments

--- verify.js
+++ verify.js
@@ -13,25 +13,33 @@ form.addEventListener("submit", handleFormSubmit);
 async function handleFormSubmit(event) {
   event.preventDefault();
   clearError();
+  //Check Title to make sure it has 5 or more characters.
   if (title.value.length < 5) {
     setError("Title < 5 Characters!");
 	return;
   }
+  //Check Content message to make sure it has 5 or more characters.
   if (content.value.length < 5) {
     setError("Content < 5 Characters!");
 	return;
   }
+  //Get the current active tab.
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  //Check active tab URL to make sure it is an Unreal Engine Marketplace page.
   if (tab.url.search("unrealengine.com/marketplace") < 0) {
     setError("Tab Not UE Marketplace!");
 	return;
   }
+  //Get the bearer token of the logged in user.
   const bearer = await chrome.cookies.get({url:"https://www.unrealengine.com", name:"EPIC_BEARER_TOKEN"});
+  //Check if user is currently logged in.
   if (!bearer) {
     setError("Not Logged In!");
 	return;
   }
+  //Get the api authentication token.
   const cookie = await chrome.cookies.get({url:"https://www.unrealengine.com/marketplace", name:"XSRF-TOKEN"});
+  //Check if user has a valid token.
   if (!cookie) {
     setError("Not Authorized!");
 	return;
@@ -42,7 +50,9 @@ async function handleFormSubmit(event) {
       tabId: tab.id,
     },
     func: (arg1, arg2, arg3, arg4) => {
+      //Function used to add a review.
       var addReview = function(product, owner, token, stars, subject, message) {
+        //Add the review exactly as it was filled out in the extension's form.
         fetch("https://www.unrealengine.com/marketplace/api/review/" + product + "/reviews/add", {
           method: "POST",
           headers: {
@@ -52,6 +62,7 @@ async function handleFormSubmit(event) {
           },
           body: JSON.stringify({ "rating": stars, "title": subject, "content": message, "targetOwner": owner })
         });
+        //Overwrite the review to make it look as though it was submitted from the Unreal Engine Marketplace.
 		fetch("https://www.unrealengine.com/marketplace/api/review/" + product + "/reviews/add", {
           method: "POST",
           headers: {
@@ -61,6 +72,7 @@ async function handleFormSubmit(event) {
           },
           body: JSON.stringify({ "rating": stars, "title": "-----", "content": "-----", "targetOwner": owner })
         });
+        //Attempt to add a question, which failed since Epic Games completely removed the logic for adding questions.
         /*fetch("https://www.unrealengine.com/marketplace/api/review/" + product + "/questions/add", {
           method: "POST",
           headers: {
@@ -71,15 +83,20 @@ async function handleFormSubmit(event) {
           body: JSON.stringify({ "title": subject, "content": message, "targetOwner": owner })
         });*/
       }
+      //Get the source HTML of the current active tab.
       const html = document.documentElement.outerHTML;
+      //Find the productID of the product page.
       const parts = html.split("\"productID\":\"");
       const product = parts.pop().split("\"").shift();
+      //Get the product information.
       var request = new XMLHttpRequest();
       request.open("GET", "https://www.unrealengine.com/marketplace/api/assets/asset/" + product, true);
       request.send(null);
       request.onreadystatechange = function() {
-        if (request.readyState == 4)
+        if (request.readyState == 4) {
+          //Get the owner ID of the product and then add the review.
           addReview(product, (request.responseText.split("\"owner\":\"")[1]).split("\"").shift(), arg1, arg2, arg3, arg4);
+        }
       };
     }
   });
